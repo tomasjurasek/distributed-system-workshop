5101bffac32c8c5686c9a0d882f7fd968fa0832a
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2022-02-07 21:25:49 +0100

    Update

--- Solutions/Writer/Writer.Domain/Validators/OrderValidator.cs
+++ Solutions/Writer/Writer.Domain/Validators/OrderValidator.cs
@@ -3,7 +3,7 @@ namespace Writer.Domain.Validators;
 
 public class OrderValidator<TAggregate> where TAggregate : IAggregateRoot
 {
-    public ICollection<string> GetValidationErrors<TAggregate, CreateOrder>(TAggregate aggregate, CreateOrder command)
+    public ICollection<string> GetValidationErrors<CreateOrder>(TAggregate aggregate, CreateOrder command)
     {
         return Array.Empty<string>();
     }
