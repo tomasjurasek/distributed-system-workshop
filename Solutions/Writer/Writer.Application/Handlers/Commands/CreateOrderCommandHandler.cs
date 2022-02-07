52c7e43ede240eaa517114322357820bc182c389
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2022-02-07 21:40:34 +0100

    MassTransit

---
+++ Solutions/Writer/Writer.Application/Handlers/Commands/CreateOrderCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Writer.Application.Interfaces;
+using Writer.Domain.Aggregates;
+using Writer.Domain.Commands;
+
+namespace Writer.Application.Handlers.Commands;
+
+internal class CreateOrderCommandHandler : ICommandHandler<Order, CreateOrder>
+{
+    public Task Consume(ConsumeContext<CreateOrder> context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<string>> GetValidationErrorsAsync(CreateOrder command)
+    {
+        throw new NotImplementedException();
+    }
+}
+
--- Solutions/Writer/Writer.Application/Interfaces/ICommandHandler.cs
+++ Solutions/Writer/Writer.Application/Interfaces/ICommandHandler.cs
@@ -1,13 +1,14 @@
-﻿using Writer.Domain.Aggregates;
+﻿using MassTransit;
+using Writer.Domain.Aggregates;
 using Writer.Domain.Commands;
 namespace Writer.Application.Interfaces;
 
-public interface ICommandHandler<TAggregate, TCommand>
+public interface ICommandHandler<TAggregate, TCommand> : IConsumer<TCommand>
     where TAggregate : IAggregateRoot
-    where TCommand : ICommand
+    where TCommand : class, ICommand
+    
 {
     Task<ICollection<string>> GetValidationErrorsAsync(TCommand command);
 
-    Task HandleAsync(TCommand command);
 }
 
