f094c59bb62f84fe6ace55df14fb91dbb69a687f
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2023-02-26 21:55:44 +0100

    Update

---
+++ Solutions/Solution/Order.Write.ProcessManager/Program.cs
@@ -0,0 +1,10 @@
+using Order.Write.ProcessManager;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
